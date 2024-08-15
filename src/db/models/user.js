cf0177a72b6828cf42a1bb3cad252aab19f5e81e
Iryna P <irpolg@ukr.net>
2024-08-15 21:32:13 +0300

    5hw-nodejs-2krok-models-user-session

---
+++ src/db/models/user.js
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const UsersCollection = model('user', userSchema);
+//прийнято, що назва схеми userSchema - однина!!!
